052323d9770e09ac591f8643f4b602309fa02cb7
slyngstad60 <kevan.slyngstad@geckodesigns.com>
2018-04-17 16:48:29 -0600

    Added some styles

--- src/server/routes/user/team.js
+++ src/server/routes/user/team.js
@@ -7,10 +7,10 @@ const authController = require('../../controllers/AuthController');
 const userController = require('../../controllers/UserController');
 const teamController = require('../../controllers/TeamController');
 
-// router.get('/teams',
-// 	catchErrors(teamController.getUserTeam),
-// 	catchErrors(teamController.getTeams)
-// );
+router.get('/teams',
+	catchErrors(teamController.getUserTeam),
+	catchErrors(teamController.getTeams)
+);
 
 router.get('/teams/create',
 	authController.isLoggedIn,
